f9bc8f465f65171597e0967e9d30e38436bb58ad
dmitry-blind-pew <dmitry.ol.bat@gmail.com>
2025-12-04 21:06:40 +0300

    add for tests db_manager and async_client

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,41 +2,60 @@ import json
 
 import pytest
 from httpx import AsyncClient
-from sqlalchemy.dialects.mysql import insert
 
 from src.config import settings
-from src.database import BaseORM, engine_null_pool
+from src.database import BaseORM, engine_null_pool, async_session_maker_null_pool
 from src.main import app
 from src.models import *
+from src.schemas.hotels import HotelSchemaAddData
+from src.schemas.rooms import RoomSchemaAddData
+from src.utils.db_manager import DBManager
+
 
 @pytest.fixture(scope="session", autouse=True)
 async def check_test_mode():
     assert settings.MODE == "TEST"
+
+
+@pytest.fixture(scope="function", autouse=True)
+async def db_manager():
+    async with DBManager(session_factory=async_session_maker_null_pool) as db:
+        yield db
+
 
 @pytest.fixture(scope="session", autouse=True)
 async def setup_database(check_test_mode):
     async with engine_null_pool.begin() as conn:
         await conn.run_sync(BaseORM.metadata.drop_all)
         await conn.run_sync(BaseORM.metadata.create_all)
-        with (
-            open("tests/test_data/mock_hotels.json", "r", encoding="utf-8") as hotels_file,
-            open("tests/test_data/mock_rooms.json", "r", encoding="utf-8") as rooms_file
-        ):
-            hotels_data = json.load(hotels_file)
-            rooms_data = json.load(rooms_file)
-        await conn.execute(insert(HotelsORM).values(hotels_data))
-        await conn.execute(insert(RoomsORM).values(rooms_data))
-        await conn.commit()
 
+    with (
+        open("tests/test_data/mock_hotels.json", "r", encoding="utf-8") as hotels_file,
+        open("tests/test_data/mock_rooms.json", "r", encoding="utf-8") as rooms_file
+    ):
+        hotels_data = json.load(hotels_file)
+        rooms_data = json.load(rooms_file)
+    hotels_schemas = [HotelSchemaAddData.model_validate(hotel) for hotel in hotels_data]
+    rooms_schemas = [RoomSchemaAddData.model_validate(room) for room in rooms_data]
+
+    async with DBManager(session_factory=async_session_maker_null_pool) as db:
+        await db.hotels.add_bulk(hotels_schemas)
+        await db.rooms.add_bulk(rooms_schemas)
+        await db.commit()
+
+
+@pytest.fixture(scope="session")
+async def async_client():
+    async with AsyncClient(app=app, base_url="http://test") as aclient:
+        yield aclient
 
 
 @pytest.fixture(scope="session", autouse=True)
-async def create_user(setup_database):
-    async with AsyncClient(app=app, base_url="http://test") as client:
-        await client.post(
-            "/auth/register",
-            json={
-                "email": "test@gmail.com",
-                "password": "1234",
-            }
-        )
+async def create_user(async_client, setup_database):
+    await async_client.post(
+        "/auth/register",
+        json={
+            "email": "test@gmail.com",
+            "password": "1234",
+        }
+    )
--- tests/integration_tests/hotels/test_db.py
+++ tests/integration_tests/hotels/test_db.py
@@ -1,11 +1,8 @@
-from src.database import async_session_maker_null_pool
 from src.schemas.hotels import HotelSchemaAddData
-from src.utils.db_manager import DBManager
 
 
-async def test_add_hotel():
+async def test_add_hotel(db_manager):
     data = HotelSchemaAddData(title="Test Hotel", location="New York, Palm str. 23")
-    async with DBManager(session_factory=async_session_maker_null_pool) as db:
-        result = await db.hotels.add(data)
-        await db.commit()
-        print(result)
+    result = await db_manager.hotels.add(data)
+    await db_manager.commit()
+    print(result)
